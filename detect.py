f9b8939970d8c4073b33a0c057ad67ec8c1f7ad6
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-09-29 18:44:22 +0000

    train yolov8n with 10 epochs

---
+++ detect.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+
+# Dataset
+dataset = '/root/khanhdq/Hand_Gesture/datasets/HAGRID_YOLO/hagrid.yaml'
+
+# Load model
+model = YOLO('yolov8n.pt')
+
+# Start training
+model.train(
+    task = 'detect',
+    data = dataset, 
+    imgsz = (640, 480),
+    epochs = 10, 
+    lr0 = 0.001, 
+    device = [0, 1]
+)
