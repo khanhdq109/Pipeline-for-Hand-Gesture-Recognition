96a64dd480d0b80de6eb3d4570bdcd8c707e55dd
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-10-04 16:09:09 +0700

    add data downloader from kaggle

---
+++ test.py
@@ -0,0 +1,15 @@
+import kaggle
+
+# Install the Kaggle API client
+kaggle.api.authenticate()
+
+# Specify the dataset you want to download (replace with the dataset name)
+dataset_name = 'kylecloud/20bn-jester-v1-videos' # Format: 'username/dataset-name'
+
+# Set the destination folder where you want to save the dataset
+destination_folder = '../../datasets/JESTER-V1/images'
+
+# Download the dataset
+print('Downloading the dataset...')
+kaggle.api.dataset_download_files(dataset_name, path = destination_folder, unzip = False)
+print('Dataset downloaded successfully!')
