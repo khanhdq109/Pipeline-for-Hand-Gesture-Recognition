b3e6eed2e19109c977bb79dfa6aafd25a2014663
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-10-10 14:24:43 +0000

    train with total 60 epochs

--- detect_train.py
+++ detect_train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 dataset = '/root/Hand_Gesture/datasets/HAGRID_YOLO-V1/hagrid.yaml'
 
 # Load model
-model = YOLO('model/detect/v8n_30.pt')
+model = YOLO('model/detect/v8n_50.pt')
 
 # Start training
 model.train(
