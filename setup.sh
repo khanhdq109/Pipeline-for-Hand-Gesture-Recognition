d3182816a8124ec1d9b87f857bc182aea5bb6f54
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-10-30 10:57:05 +0700

    construct setup.sh - demo

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#######################################
+###### Quickly setup the server #######
+#######################################
+
+# Install packages
+pip install ultralytics
+pip install scikit-image
+pip install kaggle
+
+sudo apt update
+sudo apt search vim
+sudo apt install vim
+
+sudo apt-get install libl1-mesa-glx
+sudo ldconfig
+ls -l /usr/lib/x86_64-linux-gnu/libGL.so.1
+
+# Setup directory
+cd ..
+mkdir datasets
+cd datasets
+mkdir JESTER-V1
+cd JESTER-V1
+mkdir images annotations
+cd ../../..
+
+# Setup KAGGLE
+export KAGGLE_CONFIG_DIR=/root/Hand_Gesture/src/data_preprocessing
+chmod 600 /root/Hand_Gesture/src/data_preprocessing/kaggle.json
+cd data_preprocessing
+
+# Download and setup JESTER-V1
+chmod +x jester_preprocessing.sh
+./jester_preprocessing.sh
+rm /root/Hand_Gesture/datasets/JESTER-V1/images/20bn-jester-v1-videos.zip
