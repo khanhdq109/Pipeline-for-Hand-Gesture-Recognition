3e71008d88ec198a28def16b86cb0682a02b2fe2
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-11-30 21:32:03 +0700

    Change batch size to 2

--- classify_train.py
+++ classify_train.py
@@ -99,7 +99,7 @@ print('Selected device:', device)
 ## Data parameters
 resize = (112, 112)
 num_frames = 30
-batch_size = 3
+batch_size = 2
 num_workers = 8 # Number of threads for data loading
 small_version = False
 ## Model parameters
