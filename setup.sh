1afd74c0e471b5113a860681cba7b514b920ee5f
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-11-30 21:07:42 +0700

    Update something that I cant remember

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 # Install packages
 pip install kaggle
-pip install numpy pandas matplotlib tqdm
+pip install numpy pandas matplotlib tqdm seaborn
 pip install opencv-python
 pip install scikit-image
 pip install scikit-learn
