44b4c40415d823c80b00f40449dd18d47344564e
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-12-06 10:18:09 +0700

    Update path:

--- data_preprocessing/jester_preprocessing.sh
+++ data_preprocessing/jester_preprocessing.sh
@@ -1,10 +1,9 @@
 # Instructions to pre-process JESTER-V1 dataset
 
 # Download dataset
+pwd
 python kaggle_download.py JESTER
 # Unzip .zip file
 python unzip_multithread.py JESTER
-# Change name of images
-# python change_name.py
 # Generate annotation files
 python gen_annotation.py
