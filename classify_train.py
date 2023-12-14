02387d933385e825328a52112489a02c870ceaa1
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-12-14 11:29:26 +0700

    4:

--- classify_train.py
+++ classify_train.py
@@ -47,7 +47,7 @@ print('Selected device:', device)
 ## Data parameters
 resize = (112, 112)
 num_frames = 30
-batch_size = 32
+batch_size = 4
 num_workers = 8 # Number of threads for data loading
 small_version = False
 ## Model parameters
