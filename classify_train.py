bcb0b1673df647185b265754e005557108eec9af
khanhdq109 <khanh.daoquoc@hcmut.edu.vn>
2023-12-14 10:43:26 +0700

    Release demo version with batch size = 2

--- classify_train.py
+++ classify_train.py
@@ -46,7 +46,7 @@ print('Selected device:', device)
 ## Data parameters
 resize = (112, 112)
 num_frames = 30
-batch_size = 1 #HERE
+batch_size = 2 #HERE
 num_workers = 8 # Number of threads for data loading #HERE
 small_version = True #HERE
 ## Model parameters
